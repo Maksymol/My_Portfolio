0b336b53d884b5d964e18948950cd6d84d0dbf59
Maksym Oliynik <maksymoliynik@gmail.com>
2023-12-29 21:37:21 +0200

    first commit

---
+++ src/app/about_me/about_me.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-about_me',
+  templateUrl: './about_me.component.html',
+  styleUrls: ['./about_me.component.css']
+})
+export class About_meComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ParalaxComponent } from './paralax/paralax.component';
+import { About_meComponent } from './about_me/about_me.component';
+import { StatisticsComponent } from './statistics/statistics.component';
+import { PortfolioSectionComponent } from './portfolio-section/portfolio-section.component';
+import { ModalComponent } from './modal/modal.component';
+import { ContactsComponent } from './contacts/contacts.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [							
+    AppComponent,
+      NavbarComponent,
+      ParalaxComponent,
+      About_meComponent,
+      StatisticsComponent,
+      PortfolioSectionComponent,
+      ModalComponent,
+      ContactsComponent,
+      FooterComponent
+   ],
   imports: [
     BrowserModule,
     AppRoutingModule
